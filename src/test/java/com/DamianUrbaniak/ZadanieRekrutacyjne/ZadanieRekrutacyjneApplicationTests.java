c601fe9be6cb256f0dbcb225d10d07bb1042b37c
Damian Urbaniak <damianourbaniak@gmail.com>
2022-11-20 07:54:36 +0100

    Initial commit

---
+++ src/test/java/com/DamianUrbaniak/ZadanieRekrutacyjne/ZadanieRekrutacyjneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DamianUrbaniak.ZadanieRekrutacyjne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieRekrutacyjneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
